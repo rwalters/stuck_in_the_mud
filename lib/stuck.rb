3ec660a57d0f1ce3fc1881ca9259c82d18542fac
Ray Walters <ray.walters@gmail.com>
2019-07-11 18:11:12 -0400

    Ari's attempt at the kata

--- lib/stuck.rb
+++ lib/stuck.rb
@@ -1,5 +1,20 @@
 class Stuck
-  def roll(dice = 5)
-    Array.new(dice){ rand(1..6) }.sum
+  def initialize
+    @dice_set = Array.new(5){ rand(1..6) }
+    @score = 0
+  end
+
+  attr_reader :dice_set, :score
+
+  def over?
+    dice_set.empty?
+  end
+
+  def roll_dice
+    return nil if over?
+    dice_set.map!{ rand(1..6) }
+    dice_set.reject!{ |die| die == 2 || die == 5 }
+    @score = @score + dice_set.sum
+    dice_set
   end
 end
--- spec/stuck_spec.rb
+++ spec/stuck_spec.rb
@@ -3,7 +3,51 @@ require 'stuck'
 RSpec.describe Stuck do
   subject(:game) { described_class.new }
 
-  it "rolls dice" do
-    expect(game.roll).to be > 0
+  context "#roll_dice" do
+    it "rolls dice" do
+      expect(subject.roll_dice).to be_a Array
+    end
+
+    it "removes 2s and 5s" do
+      subject.roll_dice.each do |die|
+        expect(die).to_not eq(2)
+        expect(die).to_not eq(5)
+      end
+    end
+
+    context "when game is over" do
+      it "returns nil" do
+        allow(subject).to receive(:dice_set).and_return([])
+        expect(subject.roll_dice).to eq(nil)
+      end
+    end
+  end
+
+  context "#over?" do
+    it "retuns false if there are valid rolls left" do
+      allow(subject).to receive(:dice_set).and_return([3])
+      expect(subject.over?).to eq(false)
+    end
+
+    it "returns true if there aren't any valid rolls left" do
+      allow(subject).to receive(:dice_set).and_return([])
+      expect(subject.over?).to eq(true)
+    end
+  end
+
+  context "#score" do
+    context "when the game has started" do
+      it "retuns 0" do
+        expect(subject.score).to eq(0)
+      end
+    end
+
+    context "when the game is underway" do
+      it "returns the expected score" do
+        expect(subject.score).to eq(0)
+        subject.roll_dice
+        expect(subject.score).to be > 0
+      end
+    end
   end
 end
