eed2363954a6dc3a77a0359e857f1b7bf2460714
Ray Walters <ray.walters@gmail.com>
2019-09-10 11:49:08 -0400

    Pass in RNG to control results in testing

--- lib/stuck.rb
+++ lib/stuck.rb
@@ -1,5 +1,13 @@
 class Stuck
-  def roll(dice = Array.new(5))
-    dice.map{ rand(1..6) }
+  def initialize(rng = nil)
+    @rng = rng || Random.new
   end
+
+  def roll(dice = Array.new(5))
+    dice.map{ rng.rand(1..6) }
+  end
+
+  private
+
+  attr_reader :rng
 end
--- spec/stuck_spec.rb
+++ spec/stuck_spec.rb
@@ -1,7 +1,18 @@
 require 'stuck'
 
 RSpec.describe Stuck do
-  subject(:game) { described_class.new }
+  class FakeRand
+    def initialize(ary = nil)
+      @ary = ary || (1..99).to_a
+    end
+
+    def rand(*args)
+      @ary.shift
+    end
+  end
+
+  subject(:game) { described_class.new(rng) }
+  let(:rng) { nil }
 
   it "rolls dice" do
     expect(game.roll).to be_an Array
@@ -9,5 +20,13 @@ RSpec.describe Stuck do
 
   it "rolls the expected number of dice" do
     expect(game.roll([1,2,3]).count).to eq(3)
+  end
+
+  context "when passed an RNG" do
+    let(:rng) { FakeRand.new }
+
+    it "returns the expected roll" do
+      expect(game.roll).to eq([1,2,3,4,5])
+    end
   end
 end
